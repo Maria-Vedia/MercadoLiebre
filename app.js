c54c3eef97da78cb402737ca6ac38234624c6000
Maria Vedia Mamani <vediamarii@gmail.com>
2023-09-03 16:32:24 -0300

    solo falta arreglar header y las rutas

--- app.js
+++ app.js
@@ -8,12 +8,28 @@ const publicPath = path.resolve(__dirname, './public');
 
 app.use(express.static(publicPath));
 
-app.listen(3010, () => {
+app.listen(3001, () => {
     console.log('Servidor andando');
 });
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'views/index.html'));
+    const home1 = path.resolve(__dirname,'./views/index.html');
+     res.sendFile(home1);
+  }); 
+  
+  app.get('/login', (req, res) => {
+    const login = path.resolve(__dirname,'./views/login.html');
+     res.sendFile(login);
+  }); 
+
+  app.get('/register', (req, res) => {
+    const register = path.resolve(__dirname,'./views/register.html');
+     res.sendFile(register);
+  }); 
+
+/*
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './views/index.html'));
 }); 
 
 app.get('/', (req, res) => {
@@ -22,4 +38,4 @@ app.get('/', (req, res) => {
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'views/register.html'));
-});
+});*/
