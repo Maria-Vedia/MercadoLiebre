f31905ba203dc344925424313380675b0fcfcab8
Maria Vedia Mamani <vediamarii@gmail.com>
2023-09-05 00:11:00 -0300

    ultimo cambio

--- app.js
+++ app.js
@@ -8,18 +8,18 @@ const publicPath = path.resolve(__dirname, './public');
 
 app.use(express.static(publicPath));
 
-app.listen(3001, () => {
-    console.log('Servidor andando');
-});
-
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, './views/index.html'));
 }); 
 
-app.get('/', (req, res) => {
+app.get('/login', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'views/login.html'));
 });
 
-app.get('/', (req, res) => {
+app.get('/register', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'views/register.html'));
+});
+
+app.listen(3001, () => {
+    console.log('Servidor andando');
 });
